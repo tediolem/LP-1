ac6cda6dcb5394c9150c1249d223655d482d291e
tediolem <migueltediole01@gmail.com>
2026-08-13 19:29:59 -0300

    Update temperatura.java

--- temperatura.java
+++ temperatura.java
@@ -1,19 +1,42 @@
 import java.util.Scanner;
 
 public class temperatura {
-    void main(){
-        double[] temperatura = new double[12];
+    void main() {
+        double[] temperatura = new double[13];
         double soma;
-        int t;
+        int t,agra,frio;
 
-        soma=0;
+        soma = 0;
         Scanner s = new Scanner(System.in);
-        for (t = 1; t<= 12; t++){
-            IO.println("Qual a temperatura registrada no "+t+"° mes: ");
-            temperatura[t]=s.nextDouble();
-            soma = temperatura[t]+soma;
+        for (t = 1; t <= 12; t++) {
+            IO.println("Qual a temperatura registrada no " + t + "° mes: ");
+            temperatura[t] = s.nextDouble();
+            soma = temperatura[t] + soma;
         }
 
-
+        IO.println("==============================================");
+        agra= 0;
+        frio= 0;
+        for (t = 1; t <= 12; t++) {
+            if (temperatura[t] > 18){
+                IO.println(t + "° mês a temperatura foi agradável.");
+                agra= agra+1;
+            }
+            else{
+            IO.println(t + "° mês a temperatura foi frio.");
+            frio= frio+1;
+            }
+        }
+        if (agra>frio){
+            IO.println("==============================================");
+            IO.println("São José é uma cidade com temperatura mais agradável");
+            IO.println("==============================================");
+        }
+        else {
+            IO.println("==============================================");
+            IO.println("São José é uma cidade com temperatura mais fria");
+            IO.println("==============================================");
+        }
     }
+
 }
