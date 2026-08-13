ef25b094378b7f45c2ccd685b00a29d46094bcf7
tediolem <migueltediole01@gmail.com>
2026-08-13 16:24:35 -0300

    Add files via upload

---
+++ Camelo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Camelo {
+    void main(){
+        int camelos,x,soma,primeiro,segundo,terceiro;
+        Scanner s=new Scanner(System.in);
+
+        camelos= 35+1;
+
+        primeiro= camelos/2;
+        segundo=  camelos/3;
+        terceiro= camelos/9;
+
+        IO.println(primeiro+"    "+segundo+"     "+terceiro);
+
+
+
+
+    }
+}
